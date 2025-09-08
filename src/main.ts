c42cd446e3a8d42ff84c70a41c634378396747dd
Valentin Paris <valentinparis97@gmail.com>
2025-09-07 21:52:59 -0300

    cambio en el puerto que se escucha

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Habilita CORS para el frontend
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
-  await app.listen(3000);
+  
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
 }
 bootstrap();
