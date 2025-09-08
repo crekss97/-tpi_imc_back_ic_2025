472eb12b653b08e1ca85a19a37e5388ce8553350
Valentin Paris <valentinparis97@gmail.com>
2025-09-07 23:25:26 -0300

    changes

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import * as express from 'express';
+import express from 'express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
