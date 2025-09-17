4c38abc739375776f97cb999aa5485f3583520f1
Gaspar Scienza <gasparscienza@hotmail.com>
2025-09-17 19:46:16 -0300

    cambio de autenticacion en cada controlador

--- src/module/imc/imc.controller.ts
+++ src/module/imc/imc.controller.ts
@@ -5,19 +5,23 @@ import {
   ValidationPipe,
   Request,
   Get,
+  UseGuards,
 } from '@nestjs/common';
 import { ImcService } from './imc.service';
 import { CalcularImcDto } from './dto/calcular-imc-dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('imc')
 export class ImcController {
   constructor(private readonly imcService: ImcService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('calcular')
   calcular(@Body(ValidationPipe) data: CalcularImcDto, @Request() req) {
     return this.imcService.calcularImc(data, req);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   traerHistorial(@Request() req) {
     return this.imcService.traerHistorial(req);
