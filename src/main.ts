02de9a2aaf0c96dd29cecb05c9efd60906f6f7e9
Valentin Paris <valentinparis97@gmail.com>
2025-09-17 19:37:32 -0300

    cambio en url

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://tpi-imc-back-ic-2025.vercel.app'],
+    origin: ['https://tpi-imc-ic-2025.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
